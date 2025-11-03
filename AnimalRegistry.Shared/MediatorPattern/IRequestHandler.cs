532d4ebe8a2594c117f35d1a6509a578b459b475
Michal Witko <m.witko2000@gmail.com>
2025-11-03 18:17:26 +0100

    feat: add implicit use

--- AnimalRegistry.Shared/MediatorPattern/IRequestHandler.cs
+++ AnimalRegistry.Shared/MediatorPattern/IRequestHandler.cs
@@ -1,5 +1,8 @@
+using JetBrains.Annotations;
+
 namespace AnimalRegistry.Shared.MediatorPattern;
 
+[UsedImplicitly(ImplicitUseTargetFlags.WithInheritors)]
 public interface IRequestHandler<in TRequest, TResponse> where TRequest : IRequest<TResponse>
 {
     Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken);
