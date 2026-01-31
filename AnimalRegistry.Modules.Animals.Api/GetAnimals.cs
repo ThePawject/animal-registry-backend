6a2e63c87a72f6713681dfc0b7f25835a0fe725b
Konrad <k.szpytma3@gmail.com>
2026-01-31 21:10:06 +0100

    feat: added auth0 integration

--- AnimalRegistry.Modules.Animals.Api/GetAnimals.cs
+++ AnimalRegistry.Modules.Animals.Api/GetAnimals.cs
@@ -7,7 +7,6 @@ public class GetAnimals : Endpoint<EmptyRequest, string>
     public override void Configure()
     {
         Get(GetAnimalsRequest.Route);
-        AllowAnonymous();
     }
     
     public override Task HandleAsync(EmptyRequest req, CancellationToken ct)
--- AnimalRegistry/Program.cs
+++ AnimalRegistry/Program.cs
@@ -1,6 +1,10 @@
 using AnimalRegistry.Modules.Animals;
 using AnimalRegistry.Shared;
+using AnimalRegistry.Shared.CurrentUser;
 using FastEndpoints;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Scalar.AspNetCore;
 
 var modules = new List<IModule> { new AnimalsModule() };
@@ -8,20 +12,47 @@ var modules = new List<IModule> { new AnimalsModule() };
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<ICurrentUser, CurrentUser>();
+
+var domain = builder.Configuration["Auth0:Domain"];
+var audience = builder.Configuration["Auth0:Audience"];
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.Authority = domain;
+    options.Audience = audience;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true
+    };
+});
+builder.Services.AddAuthorization();
 
 ApplyModuleServices(modules, builder);
 
 var app = builder.Build();
 
-app.UseFastEndpoints();
-
 if (app.Environment.IsDevelopment())
 {
+    IdentityModelEventSource.LogCompleteSecurityArtifact = true;
+    IdentityModelEventSource.ShowPII = true; 
     app.MapOpenApi();
     app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseFastEndpoints();
 
 app.Run();
 return;
