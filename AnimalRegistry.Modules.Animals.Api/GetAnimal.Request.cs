88bc1ad781cb1eacf29bb72c47001ce5928d4d90
Michal Witko <m.witko2000@gmail.com>
2026-01-31 18:46:51 +0100

    fix(api): update GetAnimalRequest route and improve project file structure

--- AnimalRegistry.Modules.Animals.Api/GetAnimal.Request.cs
+++ AnimalRegistry.Modules.Animals.Api/GetAnimal.Request.cs
@@ -2,8 +2,8 @@ namespace AnimalRegistry.Modules.Animals.Api;
 
 public sealed class GetAnimalRequest
 {
-    public const string Route = "/animals/{id:guid}";
+    public const string Route = "/animals/{id}";
     public Guid Id { get; set; }
 
-    public static string BuildRoute(Guid id) => Route.Replace("{id:guid}", id.ToString());
+    public static string BuildRoute(Guid id) => Route.Replace("{id}", id.ToString());
 }
