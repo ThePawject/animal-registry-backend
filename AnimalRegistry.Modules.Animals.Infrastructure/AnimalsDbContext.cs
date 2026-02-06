444b6ed8c18d85bda821bca6df04ef7eae498d98
Michal Witko <m.witko2000@gmail.com>
2026-02-06 15:20:44 +0100

    feat: animal photo to entity

--- AnimalRegistry.Modules.Animals.Infrastructure/AnimalsDbContext.cs
+++ AnimalRegistry.Modules.Animals.Infrastructure/AnimalsDbContext.cs
@@ -6,6 +6,7 @@ namespace AnimalRegistry.Modules.Animals.Infrastructure;
 internal sealed class AnimalsDbContext(DbContextOptions<AnimalsDbContext> options) : DbContext(options)
 {
     public DbSet<Animal> Animals => Set<Animal>();
+    public DbSet<AnimalPhoto> AnimalPhotos => Set<AnimalPhoto>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
