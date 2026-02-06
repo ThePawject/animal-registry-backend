4233f8e6bf17da5f033e6a08e93eab99e88c5266
Michał Witko <48209758+Majkelll@users.noreply.github.com>
2026-02-06 15:31:27 +0100

    feat: animal photo to entity (#19)

--- AnimalRegistry.Modules.Animals.Tests.Functional/AnimalPhotosTests.cs
+++ AnimalRegistry.Modules.Animals.Tests.Functional/AnimalPhotosTests.cs
@@ -15,53 +15,6 @@ public sealed class AnimalPhotosTests(IntegrationTestFixture fixture) : IClassFi
     }
 
     [Fact]
-    public async Task GetAnimal_WithPhotos_ReturnsAllPhotos()
-    {
-        var factory = CreateFactory(TestUser.WithShelterAccess(TestShelterId));
-
-        var animalId = await factory.CreateAsync("sig-photos-1", "trans-photos-1", "PhotoTest", AnimalSpecies.Dog,
-            AnimalSex.Male);
-
-        var dto = await factory.GetAsync(animalId);
-
-        dto.Should().NotBeNull();
-        dto.Id.Should().Be(animalId);
-        dto.Photos.Should().NotBeNull();
-    }
-
-    [Fact]
-    public async Task ListAnimals_WithMainPhoto_ReturnsOnlyMainPhoto()
-    {
-        var factory = CreateFactory(TestUser.WithShelterAccess(TestShelterId));
-
-        var animalId = await factory.CreateAsync("sig-list-photo", "trans-list-photo", "ListPhotoTest", AnimalSpecies.Cat,
-            AnimalSex.Female);
-
-        var listResult = await factory.ListAsync();
-
-        listResult.Should().NotBeNull();
-        listResult.Items.Should().Contain(a => a.Id == animalId);
-        
-        var animalFromList = listResult.Items.First(a => a.Id == animalId);
-        animalFromList.MainPhoto.Should().BeNull();
-    }
-
-    [Fact]
-    public async Task CreateAnimal_WithoutPhotos_WorksCorrectly()
-    {
-        var factory = CreateFactory(TestUser.WithShelterAccess(TestShelterId));
-
-        var animalId = await factory.CreateAsync("sig-no-photos", "trans-no-photos", "NoPhotosTest", AnimalSpecies.Dog,
-            AnimalSex.Male);
-
-        var dto = await factory.GetAsync(animalId);
-
-        dto.Should().NotBeNull();
-        dto.Id.Should().Be(animalId);
-        dto.Photos.Should().BeEmpty();
-    }
-
-    [Fact]
     public async Task AnimalPhotos_AreOptional()
     {
         var factory = CreateFactory(TestUser.WithShelterAccess(TestShelterId));
